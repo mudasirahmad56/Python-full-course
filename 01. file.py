4598bb636f15cdf3364d908baae89f6887275f15
mudasirahmad56 <mudai0791@gmail.com>
2025-08-22 10:34:02 +0500

    chapter no 9

---
+++ 01. file.py
@@ -0,0 +1,11 @@
+'''
+a = " a is very long string with emails"
+
+email = []
+3 seconds
+'''
+
+file = open("file.txt", "r")
+data = file.read()
+print (data)
+file.close()
---
+++ 02_file_write.py
@@ -0,0 +1,5 @@
+st = "salam mudasir how are you"
+
+f = open("myfile.txt", "w")
+f.write(st)
+f.close()

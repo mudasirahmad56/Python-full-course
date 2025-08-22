6f2eae9b15eb8016dbc5b6aaab06afeabf167b49
mudasirahmad56 <mudai0791@gmail.com>
2025-08-22 10:30:56 +0500

    chapter no 7

---
+++ 02_list_while_using.py
@@ -0,0 +1,6 @@
+I = [1 , "mudasir",3.4,"ali","hassan",True]
+
+i = 0
+while(i<len(I)):
+    print(I[i])
+    i += 1  
---
+++ 03_for_with_else.py
@@ -0,0 +1,7 @@
+I = [1, 2, 3, 4, 5]
+
+for item in I:
+    print(item)
+else:
+    print("done")
+   
